871cca2e46cb5c34ff98bbda60ade6373f391c07
Mihail <mihailmojsoskivr@outlook.com>
2016-10-09 13:25:23 +0200

    First Commit

---
+++ JekOS/JekOS/Kernel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Sys = Cosmos.System;
+
+namespace JekOS
+{
+    public class Kernel : Sys.Kernel
+    {
+        protected override void BeforeRun()
+        {
+            Console.WriteLine("jekOS 1.0 running");
+        }
+
+        protected override void Run()
+        {
+            string input = Console.ReadLine();
+            string[] parts = input.Split(' ');
+
+            Console.Write(parts[0]);
+            Console.ReadKey();
+        }
+    }
+}
