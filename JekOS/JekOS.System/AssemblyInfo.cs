16bed4c274d1d5ace9a0f8a17a54bef6eb58b493
Jek Hales <halesyy@gmail.com>
2016-10-09 22:59:54 -0700

    Changed the Assembly Information
    dasritecunt

--- JekOS/JekOS.System/AssemblyInfo.cs
+++ JekOS/JekOS.System/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using Cosmos.Common;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Package Name")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("jekOS")]
+[assembly: AssemblyDescription("Simple COSMOS OS")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Company")]
-[assembly: AssemblyProduct("Package Name")]
+[assembly: AssemblyCompany("Jekoder")]
+[assembly: AssemblyProduct("jekOS")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
